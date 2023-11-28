4332956b0ebd7875cfe1306aa24c254d459f8c9f
lachho <143239409+lachho@users.noreply.github.com>
2023-11-28 12:12:58 +1100

    Update defence.c

--- defence.c
+++ defence.c
@@ -1,5 +1,4 @@
-// This program was written by Lachlan Ho (z5361811)
-// on 28/03/2023
+// This program was written 28/03/2023
 //
 // Simulates a Tower Defence Game by continuously taking in set commands from 
 // the user. Initial data such as initial conditions (lives, money, enemies),
